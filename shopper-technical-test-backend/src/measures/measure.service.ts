5823fdf1d283dddc59ccf4969e05d5f2ee6b4fd6
Nícolas <nicolasanunes@hotmail.com>
2024-08-28 19:29:45 -0300

    upload file to LLM and result expected implemented

--- shopper-technical-test-backend/src/measures/measure.service.ts
+++ shopper-technical-test-backend/src/measures/measure.service.ts
@@ -1,11 +1,16 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { CreateMeasureDto } from './dtos/create-measure.dto';
 
-import * as fs from 'fs';
+import { parse } from 'file-type-mime';
+import { writeFileSync } from 'fs';
 import { v4 as uuidv4 } from 'uuid';
 import { Repository } from 'typeorm';
 import { MeasureEntity } from './entities/measure.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+
+import { GoogleAIFileManager } from '@google/generative-ai/server';
+import { GoogleGenerativeAI } from '@google/generative-ai';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class MeasureService {
@@ -14,19 +19,57 @@ export class MeasureService {
     private readonly measureEntity: Repository<MeasureEntity>,
     @Inject('filePath')
     private readonly filePath: string,
+    private readonly configService: ConfigService,
   ) {}
 
-  createMeasure(createMeasureDto: CreateMeasureDto) {
-    this.saveBase64ToAFile(createMeasureDto.image);
+  async createMeasure(createMeasureDto: CreateMeasureDto) {
+    const file = await this.saveBase64ToAFile(createMeasureDto.image);
+    await this.uploadFileToLLM(file);
     return 'This action adds a new measure';
   }
 
   // Other functions
-  saveBase64ToAFile(image: string) {
+  async saveBase64ToAFile(image: string) {
     const buffer = Buffer.from(image, 'base64');
+    const fileType = parse(buffer);
+    const fileMimeType = fileType.mime;
+    const fileExtension = fileType.ext;
+    const filename = `${uuidv4()}.${fileExtension}`;
+    const filePath = `${this.filePath}/${filename}`;
 
-    const filename = `${uuidv4()}.jpg`;
+    writeFileSync(filePath, buffer);
 
-    fs.writeFileSync(`${this.filePath}/${filename}`, buffer);
+    return { filename, fileMimeType };
+  }
+
+  async uploadFileToLLM(file) {
+    const fileManager = new GoogleAIFileManager(
+      this.configService.get<string>('GEMINI_API_KEY'),
+    );
+
+    const uploadResult = await fileManager.uploadFile(
+      `${this.filePath}/${file.filename}`,
+      {
+        mimeType: `${file.fileMimeType}`,
+      },
+    );
+    // View the response.
+    console.log(`Upload de imagem para o LLM realizado.`);
+
+    const genAI = new GoogleGenerativeAI(
+      this.configService.get<string>('GEMINI_API_KEY'),
+    );
+    const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+    const result = await model.generateContent([
+      'The image is supposed to be an water or gas meter. I want the awnswer as an integer with the exactly number it registered',
+      {
+        fileData: {
+          fileUri: uploadResult.file.uri,
+          mimeType: uploadResult.file.mimeType,
+        },
+      },
+    ]);
+
+    console.log(result.response.text());
   }
 }
