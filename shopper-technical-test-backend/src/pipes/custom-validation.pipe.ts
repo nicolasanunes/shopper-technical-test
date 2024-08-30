dd1f970d9434ab31793bda271f281eb0d5858947
Nícolas <nicolasanunes@hotmail.com>
2024-08-30 10:41:17 -0300

    changed CustomValidationPipe

--- shopper-technical-test-backend/src/pipes/custom-validation.pipe.ts
+++ shopper-technical-test-backend/src/pipes/custom-validation.pipe.ts
@@ -1,15 +1,19 @@
+import { ValidationError } from '@nestjs/class-validator';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpException, HttpStatus } from '@nestjs/common';
 
 export class CustomValidationPipe extends ValidationPipe {
   constructor() {
     super({
-      exceptionFactory: () => {
+      exceptionFactory: (validationErrors: ValidationError[]) => {
+        const errorMessages = validationErrors.map((error) => {
+          return `${Object.values(error.constraints).join(', ')}`;
+        });
+
         throw new HttpException(
           {
             error_code: 'INVALID_DATA',
-            error_description:
-              'Os dados fornecidos no corpo da requisição são inválidos',
+            error_description: errorMessages.join(', '),
           },
           HttpStatus.BAD_REQUEST,
         );
